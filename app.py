a471fc53f114166209bf169c3a80ccae428ee987
Krzysztof Banek <krisbanek@gmail.com>
2022-01-12 20:56:23 +0100

    Add html, css, parent

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request, redirect
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/me")
+def me():
+    return render_template("me.html")
+
+@app.route("/contact")
+def contact():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("contact.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return redirect("/")
